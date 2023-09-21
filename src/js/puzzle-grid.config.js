fa166f7f3ea978d64b655184aea70aa02cfe40e6
mi-skam <codes@miskam.xyz>
2023-09-21 21:02:20 +0200

    Section 1: 10 Shuffle the grid
    NEW:
    - Adding PuzzleGrid._shuffle to shuffle the id's of the puzzle pieces

--- src/js/puzzle-grid.config.js
+++ src/js/puzzle-grid.config.js
@@ -1,4 +1,4 @@
-export const PuzzleGridConfig = [
+export let PuzzleGridConfig = [
   { id: 1, x: -200, y: -200 }, { id: 2, x: 0, y: -200 }, { id: 3, x: 200, y: -200 },
   { id: 4, x: -200, y: 0 }, { id: 5, x: 0, y: 0 }, { id: 6, x: 200, y: 0 },
   { id: 7, x: -200, y: 200 }, { id: 8, x: 0, y: 200 }, { id: 9, x: 200, y: 200 }
--- src/js/puzzle-grid.js
+++ src/js/puzzle-grid.js
@@ -3,17 +3,31 @@ import { PuzzleGridConfig } from './puzzle-grid.config';
 import { PuzzlePiece } from './puzzle-piece';
 
 export class PuzzleGrid {
-  constructor() {
+  constructor(toShuffle = true) {
     this.container = new PIXI.Container();
     this.container.x = window.innerWidth / 2;
     this.container.y = window.innerHeight / 2;
+    this.toShuffle = toShuffle;
     this.createPuzzlePieces();
+  }
+
+  _shuffle(grid) {
+    return grid.reduce((acc, curr, idx, grid) => {
+      const randomIdx = Math.floor(Math.random() * grid.length);
+      // swap the id's but not the rest of the object
+      [grid[idx].id, grid[randomIdx].id] = [grid[randomIdx].id, grid[idx].id];
+      return grid;
+    }, [...grid])
+
   }
 
   createPuzzlePieces() {
     this.pieces = [];
 
-    PuzzleGridConfig.forEach(({ id, x, y }) => {
+    // randomize the puzzle pieces, if needed (default is true)
+    const puzzleGrid = this.toShuffle ? this._shuffle(PuzzleGridConfig) : PuzzleGridConfig;
+
+    puzzleGrid.forEach(({ id, x, y }) => {
       const piece = new PuzzlePiece(id, x, y);
       this.container.addChild(piece.sprite);
       this.pieces.push(piece);
