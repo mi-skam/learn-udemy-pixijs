17b16449cffc1c0c79c78f774dab7873ba4287be
mi-skam <codes@miskam.xyz>
2023-09-25 19:50:43 +0200

    Section 1: swap fields

--- src/js/puzzle-grid.js
+++ src/js/puzzle-grid.js
@@ -1,38 +1,61 @@
-import * as PIXI from 'pixi.js';
-import { PuzzleGridConfig } from './puzzle-grid.config';
-import { PuzzlePiece } from './puzzle-piece';
+import * as PIXI from "pixi.js";
+import { PuzzleGridConfig } from "./puzzle-grid.config";
+import { PuzzlePiece } from "./puzzle-piece";
 
 export class PuzzleGrid {
-  constructor(toShuffle = true) {
+  constructor(isRandom = true) {
     this.container = new PIXI.Container();
     this.container.x = window.innerWidth / 2;
     this.container.y = window.innerHeight / 2;
     // enable sortableChildren to be able to sort the children by zIndex
     this.container.sortableChildren = true;
-    this.toShuffle = toShuffle;
-    this.createPuzzlePieces();
+    this.createPuzzlePieces(isRandom);
   }
 
   _shuffle(grid) {
-    return grid.reduce((acc, curr, idx, grid) => {
-      const randomIdx = Math.floor(Math.random() * grid.length);
-      // swap the id's but not the rest of the object
-      [grid[idx].id, grid[randomIdx].id] = [grid[randomIdx].id, grid[idx].id];
-      return grid;
-    }, [...grid])
-
+    return grid.reduce(
+      (acc, curr, idx, grid) => {
+        const randomIdx = Math.floor(Math.random() * grid.length);
+        // swap the id's but not the rest of the object
+        [grid[idx].id, grid[randomIdx].id] = [grid[randomIdx].id, grid[idx].id];
+        return grid;
+      },
+      [...grid]
+    );
   }
 
-  createPuzzlePieces() {
+  createPuzzlePieces(isRandom) {
     this.pieces = [];
 
     // randomize the puzzle pieces, if needed (default is true)
-    const puzzleGrid = this.toShuffle ? this._shuffle(PuzzleGridConfig) : PuzzleGridConfig;
+    const puzzleGrid = isRandom
+      ? this._shuffle(PuzzleGridConfig)
+      : PuzzleGridConfig;
 
     puzzleGrid.forEach(({ id, x, y }) => {
       const piece = new PuzzlePiece(id, x, y);
       this.container.addChild(piece.sprite);
       this.pieces.push(piece);
-    })
+
+      piece.on("dragend", () => this.onPieceDragend(piece));
+    });
+  }
+  onPieceDragend(piece) {
+    const pieceToReplace = this.pieces.find(
+      (item) =>
+        item !== piece &&
+        piece.sprite.x >= item.left &&
+        piece.sprite.x <= item.right &&
+        piece.sprite.y >= item.top &&
+        piece.sprite.y <= item.bottom
+    );
+    if (pieceToReplace) {
+      const replaceField = pieceToReplace.field;
+      pieceToReplace.setField(piece.field);
+      piece.setField(replaceField);
+      
+    } else {
+      piece.resetPosition();
+    }
   }
 }
--- src/js/puzzle-piece.js
+++ src/js/puzzle-piece.js
@@ -1,8 +1,9 @@
-import * as PIXI from 'pixi.js';
-import { globals } from './globals';
+import * as PIXI from "pixi.js";
+import { globals } from "./globals";
 
-export class PuzzlePiece {
+export class PuzzlePiece extends PIXI.utils.EventEmitter {
   constructor(id, x, y) {
+    super();
     this.sprite = new PIXI.Sprite(globals.resources[`p0${id}`].texture);
     this.sprite.x = x;
     this.sprite.y = y;
@@ -10,7 +11,7 @@ export class PuzzlePiece {
     this.sprite.scale.set(0.99);
 
     // save the field position for later use
-    this.field = { id, x, y }
+    this.field = { id, x, y };
 
     // by default elements are not interactive for performance reasons.
     this.setInteractive();
@@ -18,10 +19,10 @@ export class PuzzlePiece {
   setInteractive() {
     this.sprite.interactive = true;
     this.sprite
-      .on('pointerdown', this.onDragStart, this)
-      .on('pointerup', this.onDragEnd, this)
-      .on('pointerupoutside', this.onDragEnd, this)
-      .on('pointermove', this.onDragMove, this);
+      .on("pointerdown", this.onDragStart, this)
+      .on("pointerup", this.onDragEnd, this)
+      .on("pointerupoutside", this.onDragEnd, this)
+      .on("pointermove", this.onDragMove, this);
   }
   onDragStart(e) {
     this.dragging = true;
@@ -29,38 +30,54 @@ export class PuzzlePiece {
     // saves the position of the touch/mouse on tap/click x
     this.touchPosition = {
       x: e.data.global.x,
-      y: e.data.global.y
-    }
-
+      y: e.data.global.y,
+    };
     // raise the z-index of the element, otherwise it would be hidden by other elements
     this.sprite.zIndex = 1;
   }
   onDragEnd() {
     this.dragging = false;
-    this.sprite.x = this.field.x;
-    this.sprite.y = this.field.y;
     // reset the z-index
     this.sprite.zIndex = 0;
+    // emit a custom event, so the parent container can listen for it
+    this.emit("dragend");
+  }
+  resetPosition() {
+    this.sprite.x = this.field.x;
+    this.sprite.y = this.field.y;
   }
   onDragMove(e) {
     if (!this.dragging) {
-      return
+      return;
     } else {
-
       const currentPosition = {
         x: e.data.global.x,
-        y: e.data.global.y
-      }
+        y: e.data.global.y,
+      };
 
       const offset = {
         x: currentPosition.x - this.touchPosition.x,
-        y: currentPosition.y - this.touchPosition.y
-      }
+        y: currentPosition.y - this.touchPosition.y,
+      };
 
       this.sprite.x = this.field.x + offset.x;
       this.sprite.y = this.field.y + offset.y;
-
     }
-
+  }
+  get left() {
+    return this.sprite.x - this.sprite.width / 2;
+  }
+  get right() {
+    return this.sprite.x + this.sprite.width / 2;
+  }
+  get top() {
+    return this.sprite.y - this.sprite.height / 2;
+  }
+  get bottom() {
+    return this.sprite.y + this.sprite.height / 2;
+  }
+  setField(field) {
+    this.field = field;
+    this.resetPosition();
   }
 }
